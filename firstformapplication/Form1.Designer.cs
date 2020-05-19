7e78d17b0f483f56f746ac7c6d2f3ffb75f033b7
소원 박 <s2018s29@e-mirim.hs.kr>
2020-05-19 15:58:50 +0900

    winform

--- firstformapplication/Form1.Designer.cs
+++ firstformapplication/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mubutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mubutton
+            // 
+            this.mubutton.Location = new System.Drawing.Point(320, 217);
+            this.mubutton.Name = "mubutton";
+            this.mubutton.Size = new System.Drawing.Size(75, 23);
+            this.mubutton.TabIndex = 0;
+            this.mubutton.Text = "박소원";
+            this.mubutton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mubutton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button mubutton;
     }
 }
 
--- firstformapplication/Form1.cs
+++ firstformapplication/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +15,21 @@ namespace firstformapplication
         public Form1()
         {
             InitializeComponent();
+
+            mubutton.Enabled = true;
+            mubutton.Text = "코드에서 변경";
+            mubutton.Width = 200;
+
+            for(int i = 0; i < 5; i++)
+            {
+                Button button = new Button();
+                Controls.Add(button);
+
+                button.Location = new Point(13, 13 +( 23 + 3)*i);
+                button.Text = "동적 생성 " + i+1+"번째";
+            }
+            
+
         }
     }
 }
